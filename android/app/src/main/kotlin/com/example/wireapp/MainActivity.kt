be96fa64591fa88a65d54c07a726982ad93b41b2
ASHOK JAIN <ashokjain@MacBook-Pro.local>
2020-07-02 12:50:33 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/wireapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wireapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/iPhoneXRXSMax112.dart
@@ -0,0 +1,257 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class iPhoneXRXSMax112 extends StatelessWidget {
+  iPhoneXRXSMax112({
+    Key key,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff008080),
+      body: Stack(
+        children: <Widget>[
+          Transform.translate(
+            offset: Offset(51.0, 46.0),
+            child: Text(
+              'Choose Product',
+              style: TextStyle(
+                fontFamily: 'American Typewriter',
+                fontSize: 42,
+                color: const Color(0xffffffff),
+                shadows: [
+                  Shadow(
+                    color: const Color(0x29000000),
+                    offset: Offset(0, 3),
+                    blurRadius: 6,
+                  )
+                ],
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Transform(
+            transform: Matrix4(1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
+                1.0, 0.0, 48.43, 155.98, 0.0, 1.0),
+            child: Container(
+              width: 132.0,
+              height: 71.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5.0),
+                color: const Color(0xff5bc0de),
+                boxShadow: [
+                  BoxShadow(
+                    color: const Color(0x29000000),
+                    offset: Offset(0, 3),
+                    blurRadius: 6,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Transform(
+            transform: Matrix4(1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
+                1.0, 0.0, 224.43, 155.98, 0.0, 1.0),
+            child: Container(
+              width: 134.0,
+              height: 71.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5.0),
+                color: const Color(0xff5bc0de),
+                boxShadow: [
+                  BoxShadow(
+                    color: const Color(0x29000000),
+                    offset: Offset(0, 3),
+                    blurRadius: 6,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Transform(
+            transform: Matrix4(1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
+                1.0, 0.0, 48.43, 298.28, 0.0, 1.0),
+            child: Container(
+              width: 132.0,
+              height: 71.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5.0),
+                color: const Color(0xff5bc0de),
+                boxShadow: [
+                  BoxShadow(
+                    color: const Color(0x29000000),
+                    offset: Offset(0, 3),
+                    blurRadius: 6,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Transform(
+            transform: Matrix4(1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
+                1.0, 0.0, 224.43, 298.28, 0.0, 1.0),
+            child: Container(
+              width: 134.0,
+              height: 71.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5.0),
+                color: const Color(0xff5bc0de),
+                boxShadow: [
+                  BoxShadow(
+                    color: const Color(0x29000000),
+                    offset: Offset(0, 3),
+                    blurRadius: 6,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Transform(
+            transform: Matrix4(1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
+                1.0, 0.0, 48.43, 430.95, 0.0, 1.0),
+            child: Container(
+              width: 132.0,
+              height: 71.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5.0),
+                color: const Color(0xff5bc0de),
+                boxShadow: [
+                  BoxShadow(
+                    color: const Color(0x29000000),
+                    offset: Offset(0, 3),
+                    blurRadius: 6,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Transform(
+            transform: Matrix4(1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
+                1.0, 0.0, 50.43, 173.98, 0.0, 1.0),
+            child: SizedBox(
+              width: 128.0,
+              child: Text(
+                'Screen with \nclamp',
+                style: TextStyle(
+                  fontFamily: 'AppleGothic',
+                  fontSize: 19,
+                  color: const Color(0xffffffff),
+                  shadows: [
+                    Shadow(
+                      color: const Color(0x29000000),
+                      offset: Offset(0, 3),
+                      blurRadius: 6,
+                    )
+                  ],
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          Transform(
+            transform: Matrix4(1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
+                1.0, 0.0, 227.43, 173.98, 0.0, 1.0),
+            child: SizedBox(
+              width: 128.0,
+              child: Text(
+                'Screen w/o \nclamp',
+                style: TextStyle(
+                  fontFamily: 'AppleGothic',
+                  fontSize: 19,
+                  color: const Color(0xffffffff),
+                  shadows: [
+                    Shadow(
+                      color: const Color(0x29000000),
+                      offset: Offset(0, 3),
+                      blurRadius: 6,
+                    )
+                  ],
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          Transform(
+            transform: Matrix4(1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
+                1.0, 0.0, 56.43, 323.29, 0.0, 1.0),
+            child: SizedBox(
+              width: 116.0,
+              child: Text(
+                'Screen sqft',
+                style: TextStyle(
+                  fontFamily: 'AppleGothic',
+                  fontSize: 19,
+                  color: const Color(0xffffffff),
+                  shadows: [
+                    Shadow(
+                      color: const Color(0x29000000),
+                      offset: Offset(0, 3),
+                      blurRadius: 6,
+                    )
+                  ],
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          Transform(
+            transform: Matrix4(1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
+                1.0, 0.0, 243.43, 323.29, 0.0, 1.0),
+            child: SizedBox(
+              width: 96.0,
+              child: Text(
+                'Chain link',
+                style: TextStyle(
+                  fontFamily: 'AppleGothic',
+                  fontSize: 19,
+                  color: const Color(0xffffffff),
+                  shadows: [
+                    Shadow(
+                      color: const Color(0x29000000),
+                      offset: Offset(0, 3),
+                      blurRadius: 6,
+                    )
+                  ],
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          Transform(
+            transform: Matrix4(1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
+                1.0, 0.0, 92.43, 454.95, 0.0, 1.0),
+            child: SizedBox(
+              width: 42.0,
+              child: Text(
+                'Hex',
+                style: TextStyle(
+                  fontFamily: 'AppleGothic',
+                  fontSize: 19,
+                  color: const Color(0xffffffff),
+                  shadows: [
+                    Shadow(
+                      color: const Color(0x29000000),
+                      offset: Offset(0, 3),
+                      blurRadius: 6,
+                    )
+                  ],
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(55.5, 100.5),
+            child: SvgPicture.string(
+              _svg_p95w75,
+              allowDrawingOutsideViewBox: true,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+const String _svg_p95w75 =
+    '<svg viewBox="55.5 100.5 304.0 1.0" ><defs><filter id="shadow"><feDropShadow dx="0" dy="3" stdDeviation="6"/></filter></defs><path transform="translate(55.5, 100.5)" d="M 0 0 L 304 0" fill="none" stroke="#ffffff" stroke-width="1" stroke-miterlimit="4" stroke-linecap="butt" filter="url(#shadow)"/></svg>';
